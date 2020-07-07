aa7cd69331df15e09a2a2a017e1937e2c30cab3a
tientienmoments <60423220+tientienmoments@users.noreply.github.com>
2020-07-07 14:08:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -51,11 +51,11 @@ if (unit === "usd") {
 
         // 2nd output the receive money
         let receiveUSD = unitVnd/ exchangeRateUsd;
-        console.log("You get: " + receiveUSD.toFixed(2) + " USD")
+        console.log("You get: " + formatCurrency("usd",receiveUSD.toFixed(2))
     } else if (currency === '2') {
         let unitUsd = prompt("How much USD to change: ")
         let receiveVND = unitUsd * exchangeRateUsd;
-        console.log("You get: " + receiveVND.toFixed(2) + " VND")
+        console.log("You get: " + formatCurrency("vnd",receiveVND.toFixed(2))
     } else {
         console.log('Not available')
     }
@@ -67,18 +67,24 @@ if (unit === "usd") {
 
         // 2nd output the receive money
         let receiveEuro = unitVnd / exchangeRateEuro;
-        console.log("You get: " + receiveEuro.toFixed(2) + " USD")
+        console.log("You get: " + formatCurrency("euro",receiveEuro.toFixed(2))
     } else if (currency === '2') {
         let unitEuro = prompt("How much Euro to change: ")
         let receiveVND = unitEuro * exchangeRateEuro;
-        console.log("You get: " + receiveVND.toFixed(2) + " VND")
+        console.log("You get: " + formatCurrency("vnd",receiveVND.toFixed(2))
     } else {
         console.log('Not available')
     }
 } else {
     console.log('you need to enter the unit money')
 }
-
+function formatCurrency(type, value) {
+  const formatter = new Intl.NumberFormat(type, {
+    currency: type,
+    style: "currency"
+  });
+  return formatter.format(value);
+}
 
 
 
